6a057900819fddd75074a9afde9d4e17f334355b
fg <fg@sysadmde-MacBook-Pro.local>
2016-12-08 10:57:39 +0800

    accidentily remove git root...

---
+++ app/assets/js/library/fb_checker.js
@@ -0,0 +1,54 @@
+// appIdString = 1590515954569893; //company
+appIdString = 1760402077549165; // my own
+
+/* Facebook part? */
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id));
+	js = d.createElement(s);
+	js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+var fbInitchecker = false;
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId: appIdString,
+		cookie: true, 
+		xfbml: true, 
+		version: 'v2.5' 
+	});
+	fbInitchecker = true;
+}
+
+/* makeSureFB(successFunction, failFunction, alwaysFunctin); */
+function makeSureFB(success, fail, always) {
+	success = success ? success : function(){};
+	fail = fail ? fail : function(){};
+	always = always ? always : function(){};
+
+	if(fbInitchecker){
+		FB.getLoginStatus(function(res) {
+			res.status==='connected'
+			?FB.api('/me',function(res) {
+				res.id
+				?success()
+				:fail();
+			})
+			:FB.login(function(res) {
+				res.status==='connected'
+				?FB.api('/me',function(res) {
+					res.id
+					?success()
+					:fail();
+				})
+				:fail();
+			});
+		});
+		always();
+	}else{
+		console.log("FB initializing... please try again!");
+	}
+}
