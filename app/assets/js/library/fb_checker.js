15a4c490fccb3947e583737c7cca199ea931059f
flyc <flychung@gmail.com>
2017-03-23 18:37:04 +0800

    make jslinter works! and setting some format style of ?:

--- app/assets/js/library/fb_checker.js
+++ app/assets/js/library/fb_checker.js
@@ -1,7 +1,11 @@
+/* jshint expr : true */
+
 /* make sure FB Object is initialed and Document content is loaded */
 function allready(init) {
     /* if user didn't pass any parameter or parameter type is not function, return */
-    if (!init || typeof init !== "function") {return "please input allready function as parameter"};
+    if (!init || typeof init !== "function") {
+        return "please input allready function as parameter";
+    }
     var aCounterWhoCountForDocumentReady = 0;
 
     /* if Document content is loaded and FB Object is initialed, this function will be trigger */
@@ -17,7 +21,7 @@ function allready(init) {
             clearInterval(aIntervalWhoUseToDetectDocumentReadyOrNot);
             allreadyCallback();
         }
-    },200);
+    }, 200);
 
     /* Check FB Object is initialed or not */
     var aIntervalWhoUseToDetectFBExistOrNot = setInterval(function() {
@@ -25,7 +29,7 @@ function allready(init) {
             clearInterval(aIntervalWhoUseToDetectFBExistOrNot);
             allreadyCallback();
         }
-    },200);
+    }, 200);
 }
 
 /* make sure the DOM Object we use is perfectly exist and loaded */
@@ -33,7 +37,7 @@ function domready(cssLike, success, timeGap, times) {
     if (!cssLike) {
         return "please unique css-like selector as parameter";
     }
-    success = success ? success : function(o) {console.log(o);}
+    success = success ? success : function(o) {console.log(o); };
     timeGap = timeGap ? timeGap : 50;
     times = times ? times : 100;
 
@@ -66,11 +70,11 @@ function statusFB(success, always) {
         if (FB) {
             clearInterval(aIntervalWhoUseToDetectFBExistOrNot);
             FB.getLoginStatus(function(resL) {
-                resL.status === "connected" 
-                ? success(FB.getUserID(), FB.getAccessToken()) 
-                : resL.status === "not_authorized" 
-                    ? success(FB.getUserID(), null) 
-                    : success(null, null);
+                resL.status === "connected" ?
+                    success(FB.getUserID(), FB.getAccessToken()) 
+                : resL.status === "not_authorized" ?
+                    success(FB.getUserID(), null) 
+                : success(null, null);
             });
             always();
         }
@@ -88,39 +92,39 @@ function loginFB(success, fail, always) {
             clearInterval(aIntervalWhoUseToDetectFBExistOrNot);
             if (!FB.getUserID()) {
                 FB.login(function(resL) {
-                    resL.status === 'connected' 
-                    ? FB.api('/me', function(res) {
-                        res.id 
-                        ? success(res, resL) 
-                        : fail(res, resL);
-                    }) 
+                    resL.status === 'connected' ?
+                        FB.api('/me', function(res) {
+                            res.id ?
+                                success(res, resL) 
+                            : fail(res, resL);
+                        }) 
                     : fail(resL);
                 });
             } else {
                 if (!FB.getAccessToken()) {
                     FB.getLoginStatus(function(resL) {
-                        resL.status === 'connected' 
-                        ? FB.api('/me', function(res) {
-                            res.id 
-                            ? success(res, resL) 
-                            : fail(res, resL);
-                        }) 
-                        : FB.login(function(resL) {
-                            resL.status === 'connected' 
-                            ? FB.api('/me', function(res) {
-                                res.id 
-                                ? success(res, resL) 
+                        resL.status === 'connected' ?
+                            FB.api('/me', function(res) {
+                                res.id ?
+                                    success(res, resL) 
                                 : fail(res, resL);
-                            }) 
+                            })
+                        : FB.login(function(resL) {
+                            resL.status === 'connected' ?
+                                FB.api('/me', function(res) {
+                                    res.id ?
+                                        success(res, resL) 
+                                    : fail(res, resL);
+                                }) 
                             : fail(resL);
                         });
                     });
                 } else {
                     FB.api("/me", function(res) {
-                        res.id 
-                        ? success(res) 
+                        res.id ?
+                            success(res) 
                         : fail(res);
-                    })
+                    });
                 }
             }
         }
