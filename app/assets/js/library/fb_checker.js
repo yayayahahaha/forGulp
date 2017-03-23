3c78ae22549d3ce9fb4d248665f70ea6e47fcf99
flyc <flychung@gmail.com>
2017-03-23 15:25:30 +0800

    make statusFB more beelan

--- app/assets/js/library/fb_checker.js
+++ app/assets/js/library/fb_checker.js
@@ -1,19 +1,21 @@
 function readyDOMObjct(cssLike, success, timeGap, times) {
-    if (!cssLike) {return "please unique css-like selector";}
-    success = success ? success : function(o) { console.log(o); }
+    if (!cssLike) {
+        return "please unique css-like selector";
+    }
+    success = success ? success : function(o) {console.log(o);}
     timeGap = timeGap ? timeGap : 50;
     times = times ? times : 100;
 
     var aCounterWhoCountTimesOfTesting = 0;
     var aIntervalWhoUseToDetectDOMExistOrNot = setInterval(function() {
-        if(document.querySelector(cssLike)){
+        if (document.querySelector(cssLike)) {
             clearInterval(aIntervalWhoUseToDetectDOMExistOrNot);
-             if (document.querySelectorAll(cssLike).length == 1 ){
-                 success(document.querySelector(cssLike));
-             }else{
-                 success(document.querySelectorAll(cssLike));
-             }
-        }else{
+            if (document.querySelectorAll(cssLike).length == 1) {
+                success(document.querySelector(cssLike));
+            } else {
+                success(document.querySelectorAll(cssLike));
+            }
+        } else {
             aCounterWhoCountTimesOfTesting++;
             if (aCounterWhoCountTimesOfTesting == times) {
                 clearInterval(aIntervalWhoUseToDetectDOMExistOrNot);
@@ -21,28 +23,22 @@ function readyDOMObjct(cssLike, success, timeGap, times) {
                 success(null);
             }
         }
-    },timeGap);
+    }, timeGap);
 }
 
-function statusFB(success, justLogin, fail, always) {
+function statusFB(success, always) {
     success = success ? success : function() {};
-    justLogin = justLogin ? justLogin : function() {};
-    fail = fail ? fail : function() {};
     always = always ? always : function() {};
 
-    var aIntervalWhoUseToDetectFBExistOrNot =  setInterval(function() {
-        if(FB){
+    var aIntervalWhoUseToDetectFBExistOrNot = setInterval(function() {
+        if (FB) {
             clearInterval(aIntervalWhoUseToDetectFBExistOrNot);
             FB.getLoginStatus(function(resL) {
-               resL.status === "connected" 
-               ? FB.api("/me", function(res) {
-                   res.id
-                   ? success(FB.getUserID(), res)
-                   : fail(resL, res)
-               })
-               : resL.status === "not_authorized" 
-                   ? justLogin(FB.getUserID()) 
-                   : fail(resL.status);
+                resL.status === "connected" 
+                ? success(FB.getUserID(), FB.getAccessToken()) 
+                : resL.status === "not_authorized" 
+                    ? success(FB.getUserID(), null) 
+                    : success(null, null);
             });
             always();
         }
@@ -54,26 +50,11 @@ function loginFB(success, fail, always) {
     fail = fail ? fail : function() {};
     always = always ? always : function() {};
 
-    if (!FB.getUserID()) {
-        FB.login(function(resL) {
-            resL.status === 'connected' 
-            ? FB.api('/me', function(res) {
-                res.id 
-                ? success(res, resL) 
-                : fail(res, resL);
-            }) 
-            : fail(resL);
-        });
-    } else {
-        if (!FB.getAccessToken()) {
-            FB.getLoginStatus(function(resL) {
-                resL.status === 'connected' 
-                ? FB.api('/me', function(res) {
-                    res.id 
-                    ? success(res, resL) 
-                    : fail(res, resL);
-                }) 
-                : FB.login(function(resL) {
+    var aIntervalWhoUseToDetectFBExistOrNot = setInterval(function() {
+        if (FB) {
+            clearInterval(aIntervalWhoUseToDetectFBExistOrNot);
+            if (!FB.getUserID()) {
+                FB.login(function(resL) {
                     resL.status === 'connected' 
                     ? FB.api('/me', function(res) {
                         res.id 
@@ -82,14 +63,34 @@ function loginFB(success, fail, always) {
                     }) 
                     : fail(resL);
                 });
-            });
-        } else {
-            FB.api("/me", function(res) {
-                res.id 
-                ? success(res) 
-                : fail(res);
-            })
+            } else {
+                if (!FB.getAccessToken()) {
+                    FB.getLoginStatus(function(resL) {
+                        resL.status === 'connected' 
+                        ? FB.api('/me', function(res) {
+                            res.id 
+                            ? success(res, resL) 
+                            : fail(res, resL);
+                        }) 
+                        : FB.login(function(resL) {
+                            resL.status === 'connected' 
+                            ? FB.api('/me', function(res) {
+                                res.id 
+                                ? success(res, resL) 
+                                : fail(res, resL);
+                            }) 
+                            : fail(resL);
+                        });
+                    });
+                } else {
+                    FB.api("/me", function(res) {
+                        res.id 
+                        ? success(res) 
+                        : fail(res);
+                    })
+                }
+            }
         }
-    }
+    }, 200);
     always();
 }
